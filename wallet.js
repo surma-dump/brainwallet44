86af45500be1046ffecdd76260bc76a46f50a3c6
Surma <surma@surma.link>
2017-01-11 15:58:44 -0800

    Implement sendValues()

--- wallet.js
+++ wallet.js
@@ -12,6 +12,10 @@ function takeN(gen, n) {
   return new Array(n).fill(0).map(_ => gen.next().value);
 }
 
+function pick(keys, obj) {
+  return keys.reduce((a, k) => Object.assign(a, {[k]: obj[k]}), {});
+}
+
 function blockchainQuery(addresses = nonOptional('addresses')) {
   return fetch(`https://blockchain.info/multiaddr?active=${addresses.join('|')}&cors=true`, {mode: 'cors'})
     .then(resp => resp.json());
@@ -28,13 +32,16 @@ class BIP44Wallet {
       .deriveHardened(coinType)
       .deriveHardened(account)
       .derive(change)
-      .derive(index);
+      .derive(index).keyPair;
   }
 
   async balance({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
-    const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
+    const usedAddresses = [
+      ...await this._usedAddresses({purpose, coinType, account, change: 0, offset}),
+      ...await this._usedAddresses({purpose, coinType, account, change: 1, offset}),
+    ];
     return usedAddresses
-      .map(addr => addr.final_balance)
+      .map(addr => addr.balance)
       .reduce((sum, addr) => sum + addr, 0);
   }
 
@@ -62,13 +69,18 @@ class BIP44Wallet {
       // If non of the addresses are used, we are done.
       if (usedAddresses.length === 0) return accumulatedUsedAddresses;
 
+
       Array.prototype.push.apply(
         accumulatedUsedAddresses,
         usedAddresses
-          .map(t => Object.assign(
-            t, 
-            addresses.find(a => t.address === a.address))
-          )
+          .map(ua => Object.assign(
+            // address and path
+            addresses.find(a => a.address === ua.address),
+            {balance: ua.final_balance},
+            {
+              transaction: pick(['hash', 'inputs', 'out'], transactions.txs.find(t => t.out.some(o => o.addr === ua.address)))
+            }
+          ))
           .sort((a, b) => a.path.index - b.path.index)
       );
     }
@@ -79,21 +91,20 @@ class BIP44Wallet {
     const usedAddresses2 = await this._usedAddresses({purpose, coinType, account, change: 1, offset});
     return [...usedAddresses1, ...usedAddresses2]
       .sort((a, b) => a.path.index - b.path.index)
-      .map(a => ({address: a.address, path: a.path}));
   }
 
   async nonEmptyAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
-    return usedAddresses.filter(t => t.final_balance > 0).map(t => ({address: t.address, balance: t.final_balance}));
+    return usedAddresses.filter(t => t.balance > 0)
   }
 
-  async firstUnusedIndex({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async firstUnusedIndex({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
     while (true) {
       const addresses = new Array(GAP_DETECT).fill(0)
-        .map((_, i) => wallet.keyPair({purpose, coinType, account, change: 0, index: i + offset}).getAddress());
+        .map((_, i) => wallet.keyPair({purpose, coinType, account, change, index: i + offset}).getAddress());
       const transactions = await blockchainQuery(addresses);
       const unusedIndex = addresses.findIndex(addr => transactions.addresses.find(t => t.address === addr).n_tx === 0);
-      if (unusedIndex !== -1) return offset+unusedIndex;
+      if (unusedIndex !== -1) return offset + unusedIndex;
       offset += GAP_DETECT;
     }
   }
@@ -102,25 +113,37 @@ class BIP44Wallet {
     const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
     return usedAddresses
       // get smallest amounts first so we can consolidate
-      .sort((a, b) => a.final_balance - b.final_balance)
+      .sort((a, b) => a.balance - b.balance)
       .filter(t => {
         if (value <= 0) return false;
-        value -= t.final_balance;
+        value -= t.balance;
         return true;
       })
       .map((a, i, arr) => {
-        if (i === arr.length - 1) 
-          return {
-            address: a.address,
-            path: a.path,
-            withdraw: a.final_balance + value
-          };
-        return {
-          address: a.address,
-          path: a.path,
-          withdraw: a.final_balance
-        };
+        if (i === arr.length - 1) return Object.assign(a, {withdraw: a.balance + value});
+        return Object.assign(a, {withdraw: a.balance});
       });
+  }
+
+  async sendValue(target, value, fee, {purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+    const sources = await this.assembleValue(value + fee, {purpose, coinType, account, offset});
+    const change = sources[sources.length - 1].balance - sources[sources.length - 1].withdraw;
+    const changeKey = this.keyPair({
+      purpose, coinType, account, 
+      change: 1, 
+      index: await this.firstUnusedIndex({purpose, coinType, account, change: 1})
+    });
+
+    const tx = new btc.TransactionBuilder();
+    sources.forEach((src, i) => {
+      tx.addInput(src.transaction.hash, src.transaction.out.find(o => o.addr === src.address).n)
+    });
+    tx.addOutput(target, value);
+    if (change > 0) tx.addOutput(changeKey.getAddress(), change);
+    sources.forEach((src, i) =>
+      tx.sign(i, this.keyPair(src.path))
+    );
+    return tx.build();
   }
 }
 
@@ -129,11 +152,14 @@ const seed = btc.crypto.sha256(process.argv[2]).toString('hex');
 const node = btc.HDNode.fromSeedHex(seed);
 const wallet = new BIP44Wallet(node);
 
-// wallet.balance({account: 0}).then(balance => console.log(`Total balance: ${balance} satoshi`));
+wallet.balance({account: 0}).then(balance => console.log(`Total balance: ${balance} satoshi`));
 // wallet.firstUnusedIndex({account: 0}).then(index => {
 //   console.log(`First unused transaction index: ${index} (address: ${wallet.keyPair({account: 0, change: 0, index}).getAddress()})`);
 // });
-wallet.assembleValue(204801, {account: 0}).then(keypairs => console.log(keypairs));
+// wallet.assembleValue(204801, {account: 0}).then(keypairs => console.log(keypairs));
+
 // wallet.nonEmptyAddresses({account: 0}).then(addresses => console.log(`non-empty addresses: ${JSON.stringify(addresses)}`))
 
-// wallet.usedAddresses({account: 0}).then(addr => console.log(addr));
+// wallet._usedAddresses({account: 0}).then(addr => console.log(addr));
+// wallet.sendValue('3FeF2YermL5BR8nEqHHNHghvYzxDCvY2qr', 204800, 20000, {account: 0}).then(x => console.log(x.toHex()))
+  // .catch(err => console.log(err.toString(), err.stack));
