2eff39ee45455a5c0e90380c145154723acfca78
Surma <surma@surma.link>
2017-01-11 13:29:08 -0800

    Treat change chain independently

--- wallet.js
+++ wallet.js
@@ -49,17 +49,14 @@ class BIP44Wallet {
     }
   }
 
-  async _usedAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async _usedAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
     const accumulatedUsedAddresses = [];
     let unusedCount = 0;
-    let publicAddressGen = this.addresses({purpose, coinType, account, change: 0, offset});
-    let changeAddressGen = this.addresses({purpose, coinType, account, change: 1, offset});
+    let addressGen = this.addresses({purpose, coinType, account, change, offset});
 
     while (true) {
-      const publicAddresses = takeN(publicAddressGen, GAP_DETECT);
-      const changeAddresses = takeN(changeAddressGen, GAP_DETECT);
-      const allAddresses = [...publicAddresses, ...changeAddresses];
-      const transactions = await blockchainQuery(allAddresses.map(a => a.address));
+      const addresses = takeN(addressGen, GAP_DETECT);
+      const transactions = await blockchainQuery(addresses.map(a => a.address));
 
       const usedAddresses = transactions.addresses.filter(t => t.n_tx > 0);
       // If non of the addresses are used, we are done.
@@ -70,7 +67,7 @@ class BIP44Wallet {
         usedAddresses
           .map(t => Object.assign(
             t, 
-            allAddresses.find(a => t.address === a.address))
+            addresses.find(a => t.address === a.address))
           )
           .sort((a, b) => a.path.index - b.path.index)
       );
@@ -78,8 +75,11 @@ class BIP44Wallet {
   }
 
   async usedAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
-    const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
-    return usedAddresses.map(t => t.address);
+    const usedAddresses1 = await this._usedAddresses({purpose, coinType, account, change: 0, offset});
+    const usedAddresses2 = await this._usedAddresses({purpose, coinType, account, change: 1, offset});
+    return [...usedAddresses1, ...usedAddresses2]
+      .sort((a, b) => a.path.index - b.path.index)
+      .map(a => ({address: a.address, path: a.path}));
   }
 
   async nonEmptyAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
@@ -98,14 +98,28 @@ class BIP44Wallet {
     }
   }
 
-  async gatherValue(value, {purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async assembleValue(value, {purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
     return usedAddresses
+      // get smallest amounts first so we can consolidate
       .sort((a, b) => a.final_balance - b.final_balance)
       .filter(t => {
         if (value <= 0) return false;
         value -= t.final_balance;
         return true;
+      })
+      .map((a, i, arr) => {
+        if (i === arr.length - 1) 
+          return {
+            address: a.address,
+            path: a.path,
+            withdraw: a.final_balance + value
+          };
+        return {
+          address: a.address,
+          path: a.path,
+          withdraw: a.final_balance
+        };
       });
   }
 }
@@ -119,7 +133,7 @@ const wallet = new BIP44Wallet(node);
 // wallet.firstUnusedIndex({account: 0}).then(index => {
 //   console.log(`First unused transaction index: ${index} (address: ${wallet.keyPair({account: 0, change: 0, index}).getAddress()})`);
 // });
-wallet.gatherValue(204801, {account: 0}).then(keypairs => console.log(keypairs));
+wallet.assembleValue(204801, {account: 0}).then(keypairs => console.log(keypairs));
 // wallet.nonEmptyAddresses({account: 0}).then(addresses => console.log(`non-empty addresses: ${JSON.stringify(addresses)}`))
 
-// wallet._usedAddresses({account: 0}).then(addr => console.log(addr));
+// wallet.usedAddresses({account: 0}).then(addr => console.log(addr));
