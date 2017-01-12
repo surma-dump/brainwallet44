7d531e0476c7107ccd023a1b1c2a4588de804b71
Surma <surma@surma.link>
2017-01-12 14:02:30 -0800

    Move all currency specific code into provider object

---
+++ defaultProviders.js
@@ -0,0 +1,173 @@
+const fetch = require('node-fetch');
+
+function pick(keys, obj) {
+  return keys.reduce((a, k) => Object.assign(a, {[k]: obj[k]}), {});
+}
+
+module.exports = {
+  'Bitcoin': {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    bip44: 0,
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80,
+    dustThreshold: 546, // https://github.com/bitcoin/bitcoin/blob/v0.9.2/src/core.h#L151-L162
+
+    async queryAddresses(addresses) {
+      const resp = await fetch(`https://blockchain.info/multiaddr?active=${addresses.join('|')}&cors=true`, {mode: 'cors'})
+      const data = await resp.json();
+      const newData = {
+        addresses: data.addresses.map(addr => pick(['address', 'n_tx', 'final_balance'], addr)),
+        txs: data.txs.map(tx => pick(['out', 'hash'], tx))
+      };
+      newData.txs.forEach(tx => tx.out.map(out => pick(['addr', 'n'], out)));
+      return newData;
+    },
+    async publishTx(tx) {
+      const resp = await fetch(`https://blockchain.info/pushtx?tx=${tx}`, {method: 'POST'});
+      const body = await resp.text();
+      if (!resp.ok) return Promise.reject(body);
+      return body;
+    }
+
+  },
+  'Testnet': {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    bip44: 1,
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef,
+    dustThreshold: 546
+  },
+  litecoin: {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe
+    },
+    bip44: 2,
+    pubKeyHash: 0x30,
+    scriptHash: 0x05,
+    wif: 0xb0,
+    dustThreshold: 0 // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L360-L365
+  },
+  dogecoin: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    bip44: 3,
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e,
+    dustThreshold: 0 // https://github.com/dogecoin/dogecoin/blob/v1.7.1/src/core.h#L155-L160
+  },
+  'Reddcoin': {bip44: 4},
+  'Dash': {bip44: 5},
+  'Peercoin': {bip44: 6},
+  'Namecoin': {bip44: 7},
+  'Feathercoin': {bip44: 8},
+  'Counterparty': {bip44: 9},
+  'Blackcoin': {bip44: 10},
+  'NuShares': {bip44: 11},
+  'NuBits': {bip44: 12},
+  'Mazacoin': {bip44: 13},
+  'Viacoin': {bip44: 14},
+  'ClearingHouse': {bip44: 15},
+  'Rubycoin': {bip44: 16},
+  'Groestlcoin': {bip44: 17},
+  'Digitalcoin': {bip44: 18},
+  'Cannacoin': {bip44: 19},
+  'DigiByte': {bip44: 20},
+  'Open Assets': {bip44: 21},
+  'Monacoin': {bip44: 22},
+  'Clams': {bip44: 23},
+  'Primecoin': {bip44: 24},
+  'Neoscoin': {bip44: 25},
+  'Jumbucks': {bip44: 26},
+  'ziftrCOIN': {bip44: 27},
+  'Vertcoin': {bip44: 28},
+  'NXT': {bip44: 29},
+  'Burst': {bip44: 30},
+  'MonetaryUnit': {bip44: 31},
+  'Zoom': {bip44: 32},
+  'Vpncoin': {bip44: 33},
+  'Canada eCoin': {bip44: 34},
+  'ShadowCash': {bip44: 35},
+  'ParkByte': {bip44: 36},
+  'Pandacoin': {bip44: 37},
+  'StartCOIN': {bip44: 38},
+  'MOIN': {bip44: 39},
+  'Argentum': {bip44: 45},
+  'GCRcoin': {bip44: 49},
+  'Novacoin': {bip44: 50},
+  'Asiacoin': {bip44: 51},
+  'Bitcoindark': {bip44: 52},
+  'Dopecoin': {bip44: 53},
+  'Templecoin': {bip44: 54},
+  'AIB': {bip44: 55},
+  'EDRCoin': {bip44: 56},
+  'Syscoin': {bip44: 57},
+  'Solarcoin': {bip44: 58},
+  'Smileycoin': {bip44: 59},
+  'Ether': {bip44: 60},
+  'Ether Classic': {bip44: 61},
+  'Pesobit': {bip44: 62},
+  'Open Chain': {bip44: 64},
+  'OKCash': {bip44: 69},
+  'DogecoinDark': {bip44: 77},
+  'Electronic Gulden': {bip44: 78},
+  'ClubCoin': {bip44: 79},
+  'RichCoin': {bip44: 80},
+  'Potcoin': {bip44: 81},
+  'Quarkcoin': {bip44: 82},
+  'Terracoin': {bip44: 83},
+  'Gridcoin': {bip44: 84},
+  'Auroracoin': {bip44: 85},
+  'IXCoin': {bip44: 86},
+  'Gulden': {bip44: 87},
+  'BitBean': {bip44: 88},
+  'Bata': {bip44: 89},
+  'Myriadcoin': {bip44: 90},
+  'BitSend': {bip44: 91},
+  'Unobtanium': {bip44: 92},
+  'MasterTrader': {bip44: 93},
+  'GoldBlocks': {bip44: 94},
+  'Saham': {bip44: 95},
+  'Chronos': {bip44: 96},
+  'Ubiquoin': {bip44: 97},
+  'Evotion': {bip44: 98},
+  'SaveTheOcean': {bip44: 99},
+  'BigUp': {bip44: 100},
+  'GameCredits': {bip44: 101},
+  'Dollarcoins': {bip44: 102},
+  'Zayedcoin': {bip44: 103},
+  'Dubaicoin': {bip44: 104},
+  'Stratis': {bip44: 105},
+  'Shilling': {bip44: 106},
+  'PiggyCoin': {bip44: 118},
+  'Monero': {bip44: 128},
+  'NavCoin': {bip44: 130},
+  'Factom Factoids': {bip44: 131},
+  'Factom Entry Credits': {bip44: 132},
+  'Zcash': {bip44: 133},
+  'Lisk': {bip44: 134},
+  'Steem': {bip44: 135},
+  'ZCoin': {bip44: 136},
+  'Rootstock': {bip44: 137},
+  'Giftblock': {bip44: 138},
+  'RealPointCoin': {bip44: 139},
+  'Rootstock Testnet': {bip44: 37310},
+  nameByBit44(id) {
+    return Object.entries(this).find(entry => entry[1].bip44 === id)[0];
+  }
+}
--- wallet.js
+++ wallet.js
@@ -1,6 +1,5 @@
 const btc = require('bitcoinjs-lib');
-const coinIDs = require('./slip44');
-const fetch = require('node-fetch');
+const defaultProviders = require('./defaultProviders');
 const GAP_DETECT = 20;
 
 function nonOptional(name) {
@@ -12,37 +11,30 @@ function takeN(gen, n) {
   return new Array(n).fill(0).map(_ => gen.next().value);
 }
 
-function pick(keys, obj) {
-  return keys.reduce((a, k) => Object.assign(a, {[k]: obj[k]}), {});
-}
-
-function blockchainQuery(addresses = nonOptional('addresses')) {
-  return fetch(`https://blockchain.info/multiaddr?active=${addresses.join('|')}&cors=true`, {mode: 'cors'})
-    .then(resp => resp.json());
-}
-
-async function publishTx(tx) {
-  const resp = await fetch(`https://blockchain.info/pushtx?tx=${tx}`, {method: 'POST'});
-  const body = await resp.text();
-  if (!resp.ok) return Promise.reject(body);
-  return body;
-}
-
 class BIP44Wallet {
-  constructor(node) {
+  constructor(node, providers = {}) {
     this._node = node;
+    this._providers = Object.assign({}, defaultProviders, providers);
   }
 
-  keyPair({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = nonOptional('change'), index = nonOptional('index')}) {
-    return this._node
-      .deriveHardened(purpose)
-      .deriveHardened(coinType)
-      .deriveHardened(account)
-      .derive(change)
-      .derive(index).keyPair;
+  keyPair({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), change = nonOptional('change'), index = nonOptional('index')}) {
+    return Object.assign(
+      this._node
+        .deriveHardened(purpose)
+        .deriveHardened(coinType.bip44)
+        .deriveHardened(account)
+        .derive(change)
+        .derive(index).keyPair,
+        {network: coinType}
+    );
   }
 
-  async balance({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  provider(coinType) {
+    if (coinType in this._provider) return this._provider[coinType];
+    throw new Error(`No provider for ${this._providers.nameByBit44(coinType.bit44)}`);
+  }
+
+  async balance({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const usedAddresses = [
       ...await this._usedAddresses({purpose, coinType, account, change: 0, offset}),
       ...await this._usedAddresses({purpose, coinType, account, change: 1, offset}),
@@ -52,7 +44,7 @@ class BIP44Wallet {
       .reduce((sum, addr) => sum + addr, 0);
   }
 
-  *addresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
+  *addresses({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
     let index = offset;
     while(true) {
       yield {
@@ -63,14 +55,14 @@ class BIP44Wallet {
     }
   }
 
-  async _usedAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
+  async _usedAddresses({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
     const accumulatedUsedAddresses = [];
     let unusedCount = 0;
     let addressGen = this.addresses({purpose, coinType, account, change, offset});
 
     while (true) {
       const addresses = takeN(addressGen, GAP_DETECT);
-      const transactions = await blockchainQuery(addresses.map(a => a.address));
+      const transactions = await coinType.queryAddresses(addresses.map(a => a.address));
 
       const usedAddresses = transactions.addresses.filter(t => t.n_tx > 0);
       // If non of the addresses are used, we are done.
@@ -84,16 +76,14 @@ class BIP44Wallet {
             // address and path
             addresses.find(a => a.address === ua.address),
             {balance: ua.final_balance},
-            {
-              transaction: pick(['hash', 'inputs', 'out'], transactions.txs.find(t => t.out.some(o => o.addr === ua.address)))
-            }
+            {transaction: transactions.txs.find(t => t.out.some(o => o.addr === ua.address))}
           ))
           .sort((a, b) => a.path.index - b.path.index)
       );
     }
   }
 
-  async usedAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async usedAddresses({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     return [
       ...await this._usedAddresses({purpose, coinType, account, change: 0, offset}), 
       ...await this._usedAddresses({purpose, coinType, account, change: 1, offset})
@@ -101,23 +91,23 @@ class BIP44Wallet {
       .sort((a, b) => a.path.index - b.path.index)
   }
 
-  async nonEmptyAddresses({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async nonEmptyAddresses({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const usedAddresses = await this._usedAddresses({purpose, coinType, account, offset});
     return usedAddresses.filter(t => t.balance > 0)
   }
 
-  async firstUnusedIndex({purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
+  async firstUnusedIndex({purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), change = 0, offset = 0}) {
     while (true) {
       const addresses = new Array(GAP_DETECT).fill(0)
         .map((_, i) => wallet.keyPair({purpose, coinType, account, change, index: i + offset}).getAddress());
-      const transactions = await blockchainQuery(addresses);
+      const transactions = await coinType.queryAddresses(addresses);
       const unusedIndex = addresses.findIndex(addr => transactions.addresses.find(t => t.address === addr).n_tx === 0);
       if (unusedIndex !== -1) return offset + unusedIndex;
       offset += GAP_DETECT;
     }
   }
 
-  async assembleValue(value, {purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async assembleValue(value, {purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const usedAddresses = [
       ...await this._usedAddresses({purpose, coinType, account, change: 0, offset}),
       ...await this._usedAddresses({purpose, coinType, account, change: 1, offset})
@@ -137,7 +127,7 @@ class BIP44Wallet {
       });
   }
 
-  async buildTx(target, value, fee, {purpose = 44, coinType = coinIDs['Bitcoin'], account = nonOptional('account'), offset = 0}) {
+  async buildTx(target, value, fee, {purpose = 44, coinType = defaultProviders['Bitcoin'], account = nonOptional('account'), offset = 0}) {
     const sources = await this.assembleValue(value + fee, {purpose, coinType, account, offset});
     const change = sources[sources.length - 1].balance - sources[sources.length - 1].withdraw;
     const changeKey = this.keyPair({
@@ -164,10 +154,12 @@ const seed = btc.crypto.sha256(process.argv[2]).toString('hex');
 const node = btc.HDNode.fromSeedHex(seed);
 const wallet = new BIP44Wallet(node);
 
+// console.log(wallet.keyPair({coinType: defaultProviders['Testnet'], account: 0, change: 0, index: 0}).getAddress());
 wallet.balance({account: 0}).then(balance => console.log(`Total balance: ${balance} satoshi`));
-wallet.firstUnusedIndex({account: 0}).then(index => {
-  console.log(`First unused transaction index: ${index} (address: ${wallet.keyPair({account: 0, change: 0, index}).getAddress()})`);
-});
+// wallet.balance({coinType: coinIDs['Testnet'], account: 0}).then(balance => console.log(`Total balance: ${balance} satoshi`));
+// wallet.firstUnusedIndex({coinType: coinIDs['Testnet'], account: 0}).then(index => {
+//   console.log(`First unused transaction address: ${wallet.keyPair({coinType: coinIDs['Testnet'], account: 0, change: 0, index}).getAddress()} (index: ${index})`);
+// });
 // wallet.assembleValue(204801, {account: 0}).then(keypairs => console.log(keypairs));
 
 // wallet.nonEmptyAddresses({account: 0}).then(addresses => console.log(`non-empty addresses: ${JSON.stringify(addresses)}`))
